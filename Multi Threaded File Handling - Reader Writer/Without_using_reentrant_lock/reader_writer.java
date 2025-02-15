6723fbe3542de38b7e1ef63734208aaf1b00f9f0
Romit <rombis02@gmail.com>
2025-02-15 19:02:06 +0530

    .

--- Multi Threaded File Handling - Reader Writer/Without_using_reentrant_lock/reader_writer.java
+++ Multi Threaded File Handling - Reader Writer/Without_using_reentrant_lock/reader_writer.java
@@ -15,7 +15,7 @@ class Reader implements Runnable {
 	boolean EOF;
 
 	Reader(File file) {
-		this.file = file;
+		Reader.file = file;
 		readerID = ++ReaderWriterCount.readerCount;
 		EOF = false;
 	}
@@ -23,19 +23,23 @@ class Reader implements Runnable {
 	void read() {
 		while (true) {
 			atomic.lock.readLock().lock();
+			EOF = false;
 			try (BufferedReader rd = new BufferedReader(new FileReader(file))) {
 				while (!EOF) {
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+					}
 					String content = rd.readLine();
 					EOF = content == null ? true : false;
 					System.out.println("reader " + readerID + " read: \"" + (EOF ? "\bEOF" : content + "\""));
 				}
-			} catch (IOException e) {}
-			finally {
+			} catch (IOException e) {
+			} finally {
 				atomic.lock.readLock().unlock();
 			}
 		}
 	}
-
 
 	public void run() {
 		read();
@@ -47,20 +51,30 @@ class Writer implements Runnable {
 	int writerID;
 
 	Writer(File file) {
-		this.file = file;
+		Writer.file = file;
 		writerID = ++ReaderWriterCount.writerCount;
 	}
 
 	void write() {
-		atomic.lock.writeLock().lock();
-		try(BufferedWriter wt = new BufferedWriter(new FileWriter(file, true))) {
-			String content = "hi_from_wt-" + writerID;
-			wt.newLine();
-			wt.write(content);
-			System.out.println("writer " + writerID + " wrote: \"" + content + "\"");
-		} catch (IOException e) {}
-		finally {
-			atomic.lock.writeLock().unlock();
+		while (true) {
+			atomic.lock.writeLock().lock();
+			try (BufferedWriter wt = new BufferedWriter(new FileWriter(file, true))) {
+				String content = "hi_from_wt-" + writerID;
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+				}
+				wt.newLine();
+				wt.write(content);
+				System.out.println("writer " + writerID + " wrote: \"" + content + "\"");
+			} catch (IOException e) {
+			} finally {
+				atomic.lock.writeLock().unlock();
+				try {
+					Thread.sleep(20000);
+				} catch (Exception e) {
+				}
+			}
 		}
 	}
 
@@ -72,21 +86,34 @@ class Writer implements Runnable {
 public class reader_writer {
 	static int readerID, writerID;
 
+	static void resetFile(File file) {
+		try (BufferedWriter cleaner = new BufferedWriter(new FileWriter(file))) {
+			cleaner.write("start");
+		} catch (Exception e) {
+		}
+	}
+
 	public static void main(String[] args) {
 		File file = new File("file.txt");
 
+		// FILE PREP - make fresh:
+		resetFile(file);
+
 		for (int i = 0; i < 5; i++) {
 			Thread reader_thread = new Thread(new Reader(file));
+			reader_thread.setDaemon(true); // Immediate termination after main thread(parent) terminates.
 			reader_thread.start();
 
 			Thread writer_thread = new Thread(new Writer(file));
+			writer_thread.setDaemon(true); // Immediate termination after main thread(parent) terminates.
 			writer_thread.start();
 		}
 
 		try {
-			Thread.sleep(10000);
-			//reader_thread.interrupt();
-			//writer_thread.interrupt();
-		} catch (InterruptedException e) {}
+			Thread.sleep(50000);
+		} catch (InterruptedException e) {
+		} finally {
+			System.exit(0);
+		}
 	}
 }
